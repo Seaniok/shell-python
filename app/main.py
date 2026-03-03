d58803da5b05e2622495a4aba457a393c814c887
Sean <mckinneysean91@gmail.com>
2026-03-03 20:14:30 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import shutil as sh
 
 def main():
     while True:
@@ -11,9 +11,11 @@ def main():
             break
         elif command.startswith("echo"):
             print(command[5:])
-        elif command.startswith("type"):    #rozpoznanie komendy type 
+        elif command.startswith("type"):    # Komenda *type* i rozpoznanie innych wbudowanych funkcji
             if command[5:] in builtin_commands:
                 print(f"{command[5:]} is a shell builtin")
+            elif path:= sh.which(command[5:]):  # przypisujemy zmiennej path = sh.which(command[5:]),
+                print(f"{command[5:]} is {path}")   # jeśli jest to Prawdziwe, czyli jeśli jest taki plik w PATH to zwraca pełną nazwę
             else:
                 print(f"{command[5:]}: not found")
         else:
